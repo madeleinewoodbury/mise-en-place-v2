28aea0eca0594febe551b2f0d9caab4e9b96a37f
Madeleine Woodbury <randine25@gmail.com>
2019-09-28 09:28:10 -0500

    Add route to login user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 
 // @route   GET /api/auth
@@ -15,5 +19,57 @@ router.get('/', [auth], async (req, res) => {
     res.status(500).sen('Sever Error');
   }
 });
+
+// @route   POST /api/auth
+// @desc    Authenticate user and get token
+// @acess   Public
+router.post(
+  '/',
+  [
+    check('email', 'Please enter a valid email address').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      // Check if user exists
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ error: { msg: 'Invalid credentials' } });
+      }
+
+      // Check is password is correct
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ error: { msg: 'Invalid credentials' } });
+      }
+
+      // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
