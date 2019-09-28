93fe718f026193809343ea9477180287efc86686
Madeleine Woodbury <randine25@gmail.com>
2019-09-27 23:14:23 -0500

    Add logic ro register new user and return jsonwebtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
-
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 
 // @route   POST /api/users
@@ -34,8 +36,30 @@ router.post(
         return res.status(400).json({ error: [{ msg: 'User already exist' }] });
       }
 
+      // Create instance of new user
       user = new User({ name, email, password });
-      res.json(user);
+
+      // Encrypt password and save user
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
