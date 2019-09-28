c3b0b0009aa79d849f4c20144828b60b5349a180
Madeleine Woodbury <randine25@gmail.com>
2019-09-28 09:56:09 -0500

    Add routes to view current user profile and route to create or update profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,106 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
+const config = require('config');
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
 
-// @route   GET /api/profile
-// @desc    Test route
-// @acess   Public
-router.get('/', (req, res) => {
-  res.send('Profile route');
+// @route   GET /api/profile/me
+// @desc    Get current user's profile
+// @acess   Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ id: req.user.id }).populate(
+      'user',
+      'name'
+    );
+    if (!profile) {
+      return res.status(400).json({ msg: 'No profile found' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
+
+// @route   POST /api/profile
+// @desc    Create or update profile
+// @acess   Private
+router.post(
+  '/',
+  [
+    auth,
+    check('status', 'Status is required')
+      .not()
+      .isEmpty(),
+    check('favoriteFoods', 'Favorite foods are required')
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array() });
+    }
+
+    const {
+      status,
+      website,
+      location,
+      favoriteFoods,
+      bio,
+      youtube,
+      facebook,
+      twitter,
+      instagram
+    } = req.body;
+
+    // Build profile fields
+    const profileFields = {};
+    if (status) profileFields.status = status;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (favoriteFoods) {
+      profileFields.favoriteFoods = favoriteFoods
+        .split(',')
+        .map(food => food.trim());
+    }
+    if (bio) profileFields.bio = bio;
+
+    // Build social fields
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    try {
+      let profile = await Profile.findOne({ id: req.user.id });
+
+      if (profile) {
+        // Updtae profile
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        return res.json(profile);
+      }
+
+      // Create new profile
+      profile = new Profile(profileFields);
+      await profile.save();
+      return res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
