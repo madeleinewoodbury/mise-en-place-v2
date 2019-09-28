12e2033953b6bddcb9b6f9ea85fd478c6a7dd18a
Madeleine Woodbury <randine25@gmail.com>
2019-09-27 22:20:08 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Api Running...'));
+
+const PORT = process.env.PORT || 5500;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
