5176edaa42e4fa78fdda325cd7431aa24f55a60a
Madeleine Woodbury <randine25@gmail.com>
2019-10-03 12:16:57 -0500

    Add useState hooks to Login and Register to set form data

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const handleChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    console.log('Success');
+  };
+
+  const { email, password } = formData;
+
   return (
     <section className="container">
       <div className="alert alert-danger">Invalid credentials</div>
@@ -9,17 +24,26 @@ const Login = () => {
       <p className="lead">
         <i className="fas fa-user"></i> Sign into Your Account
       </p>
-      <form className="form" action="dashboard.html">
+      <form className="form" onSubmit={e => handleSubmit(e)}>
         <div className="form-group">
           <input
             type="email"
             placeholder="Email Address"
             name="email"
+            value={email}
+            onChange={e => handleChange(e)}
             required
           />
         </div>
         <div className="form-group">
-          <input type="password" placeholder="Password" name="password" />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={e => handleChange(e)}
+            minLength={6}
+          />
         </div>
         <input type="submit" className="btn btn-primary" value="Login" />
       </form>
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const handleChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('Passwords do not match');
+    } else {
+      console.log('Success');
+    }
+  };
+
+  const { name, email, password, password2 } = formData;
+
   return (
     <section className="container">
       <h1 className="large text-primary">Sign Up</h1>
       <p className="lead">
         <i className="fas fa-user"></i> Create Your Account
       </p>
-      <form className="form" action="create-profile.html">
+      <form className="form" onSubmit={e => handleSubmit(e)}>
         <div className="form-group">
-          <input type="text" placeholder="Name" name="name" required />
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={e => handleChange(e)}
+            required
+          />
         </div>
         <div className="form-group">
-          <input type="email" placeholder="Email Address" name="email" />
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => handleChange(e)}
+          />
         </div>
         <div className="form-group">
           <input
             type="password"
             placeholder="Password"
             name="password"
-            minLength="6"
+            value={password}
+            onChange={e => handleChange(e)}
+            minLength={6}
           />
         </div>
         <div className="form-group">
@@ -28,7 +64,9 @@ const Register = () => {
             type="password"
             placeholder="Confirm Password"
             name="password2"
-            minLength="6"
+            value={password2}
+            onChange={e => handleChange(e)}
+            minLength={6}
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
