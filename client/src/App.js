e6b9d90cd6236f371042abeaaa5f78d294fabc89
Madeleine Woodbury <randine25@gmail.com>
2019-10-03 15:22:49 -0500

    Redux setup and create store

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Fragment } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+
+// Components
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
@@ -8,12 +12,16 @@ import './App.css';
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/register" component={Register} />
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </Fragment>
+    </Provider>
   );
 };
 
