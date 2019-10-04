87ea45df750caa13ac53b8907b06bff1c38af6b5
Madeleine Woodbury <randine25@gmail.com>
2019-10-03 19:51:34 -0500

    Add loadUser and action ans setAuthToken

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 // Components
 import Navbar from './components/layout/Navbar';
@@ -11,7 +13,15 @@ import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import './App.css';
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <Fragment>
