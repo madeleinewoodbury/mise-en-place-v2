e84990874f5fd0b746e73b88c4eb6fe3e6a508d9
Madeleine Woodbury <randine25@gmail.com>
2019-10-05 11:25:36 -0500

    Add Dashboard component and PrivateRoute

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -9,8 +9,8 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <ul>
       <li>
         <a onClick={logout} href="#!">
-          <i class="fas fa-sign-out-alt"></i>
-          <span class="hide-sm">Logout</span>
+          <i className="fas fa-sign-out-alt"></i>
+          <span className="hide-sm">Logout</span>
         </a>
       </li>
     </ul>
